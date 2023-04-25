c3f54f909329a407b6d599cb879bc43afd8e9b1b
RamingC <tongraming@gmail.com>
2023-04-25 12:46:59 +0700

    Last

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import './Footer.scss'
+
+type Props = {}
+
+const Footer = ({}: Props) => {
+  return (
+    <footer>
+        <p>
+            @Copyright 2023 Raming Chalermsook and Mini project react
+        </p>
+    </footer>
+  )
+}
+
+export default Footer
